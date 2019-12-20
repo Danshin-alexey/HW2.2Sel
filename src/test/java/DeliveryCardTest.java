6534774679d0dbceb86feda050ec4a5355d04788
Alexey <Ultrabob@inbox.ru>
2019-12-20 12:33:20 +0400

    Add Files

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -1,34 +1,31 @@
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardTest {
+    String dateForInput;
+
     @DisplayName("Registration form for delivery card")
     @Test
     void registrationFormForDeliveryCard() {
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 4);
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
-        String str = format1.format(c.getTime());
+        LocalDate today = LocalDate.now();
+        LocalDate dateCorrect = today.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        dateForInput = dateCorrect.format(formatter);
         open("http://localhost:9999");
-        SelenideElement form = $("form");
         $("[data-test-id=city] input").setValue("Самара");
-        $(byText("Дата встречи"));
+        $("[data-test-id=date] input").setValue(dateForInput);
         $("[data-test-id=name] input").setValue("Даньшин Алексей");
         $("[data-test-id=phone] input").setValue("+79854332355");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $("[data-test-id=notification]").waitUntil(visible, 15000);
+        $(withText("Успешно")).waitUntil(Condition.visible, 15000);
     }
+
 
 }
